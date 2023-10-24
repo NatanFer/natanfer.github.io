3107366ba4ff3a75a3a07223706f82484f6f3eb5
NatanFer <natanemanuelfernandes870@gmail.com>
2023-10-24 19:29:41 -0300

    quinta19/10

---
+++ Header/header.js
@@ -0,0 +1,19 @@
+function header(){
+    headerElement.innerHTML=`
+<header class="card1, header1">
+        <img class="img" src="/src/img/logo.svg" alt="">
+        <nav class="nav-items1 nava">
+            <strong class="buton1 to-left padding-large">Natan Emanuel Fernandes</strong>
+            <a class=" nava buton1 to-right padding-large" href="/index.html#sobre">SOBRE</a>
+            <a class=" nava buton1 to-right padding-large" href="/index.html#projetos">PROJETOS</a>
+            <a class=" nava buton1 to-right padding-large" href="/src/pages/atividades em sala/boletim.html">BOLETIM</a>
+            <a class=" nava buton1 to-right padding-large" href="/index.html#review">ARTIGOS</a>
+            <a class=" nava buton1 to-right padding-large" href="/index.html#contato">CONTATO</a>
+            <a class=" nava buton1 to-right padding-large" href="/index.html#inicio">INICIO</a>
+        </nav>
+    </header>
+`
+
+}
+
+header ()
