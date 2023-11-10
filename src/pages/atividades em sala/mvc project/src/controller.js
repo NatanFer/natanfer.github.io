b28ae7498573103ac33db66e6e51f65bbb73f183
NatanFer <natanemanuelfernandes870@gmail.com>
2023-11-09 22:18:09 -0300

    dia 09/11

--- src/pages/atividades em sala/mvc project/src/controller.js
+++ src/pages/atividades em sala/mvc project/src/controller.js
@@ -2,61 +2,77 @@ import { viewController } from "./view/viewController.js";
 import { Usuario } from "./model/usuario.model.js";
 
 let data = [];
-let submitType = {NEW:0, UPDATE:1}
+const submitType = { NEW: 0, UPDATE: 1 };
 let submitState = submitType.NEW;
 let currentId = null;
 
-
-const handleSubmit = (event) =>{
-event.preventDefault();
-const user = new Usuario(nome.value,idade.value,login.value,senha.value);
-
-if(submitState == submitType.NEW){
-addUser (user);
-}else if(submitState == submitType.UPDATE ){
-updateUser(user);
-btnSub.innerText = "NEW"
-}
-viewController.update(data, new Usuario("",0,"","",))
-}
-
-const addUser = (newUser) => {
-  data.push(newUser);
-
-  viewController.update(data, new Usuario("",0,"",""));
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const user = new Usuario(nome.value, idade.value, login.value, senha.value);
+  if (submitState == submitType.NEW) {
+    addUser(user);
+  } else if (submitState == submitType.UPDATE) {
+    updateUser(currentId, user);
+    submitState = submitType.NEW;
+    btnSub.innerText = "NEW";
+  }
+  viewController.update(data, new Usuario("", null, "", ""));
 };
 
-const updateUser = (index, userToUpdate)=>{
+//ADICIONAR NOVO USUARIO
+const addUser = (newUser) => {
+  data.push(newUser);
+};
+//ATUALIZAR USUARIO SELECIONADO
+const updateUser = (index, userToUpdate) => {
   data[index] = userToUpdate;
+};
+//DELETAR USUÁRIO SELECIONADO
+const deletUser = (index) => {
+  data.splice(index, 1);
+};
+//AÇÃO PARA BOTÃO ESQUERDO
+const clickEsquerdo = (event) => {
+  currentId = event.target.closest("tr").id.split("")[4];
+  alert(
+    `Clicou com o botão esquerdo, e o ${data[currentId]
+      .getNome()
+      .toUpperCase()} será carregado para edição`
+  );
+  viewController.updateForm(data[currentId])
+  submitState = submitType.UPDATE;
+  btnSub.innerText = "Update";
+
+};
+//AÇÃO PARA BOTÃO DIREITO
+const clickDireito = (event) => {
+  event.preventDefault();
+  if (event.button == 2) {
+    currentId = event.target.closest("tr").id.split("")[4];
+    alert(
+      `Clicou com o botão direito, e o ${data[currentId]
+        .getNome()
+        .toUpperCase()} será deletado`
+    );
+let confirm = window.confirm("certeza que vai deletar esse usuario")
+
+if (confirm){
+  deletUser(currentId);
+  viewController.update(data, new Usuario("", null, "", ""));
 }
 
-const deletUser = (index)=>{
-  data[index] = userToUpdate
-}
-
-const clickEsquerdo = (event)=>{
-  console.log=event.target.closest('tr').id.split("")[4];
-  alert(`clicou com o botão direito, e o ${data[currentId].getNome().toUpperCase()}será carregado para edição`)
-}
-  
-
-
-const clickDireito = (event)=>{
-event.preventDefault();
-if (event.button ==2){
-alert(`clicou com o botão direito, e o ${data[currentId].getNome().toUpperCase()}será deletado`)
-console.log=event.target.closest('tr').id.split("")[4];
-}
-}
+  }
+};
 const controller = {
   iniciar: () => {
     viewController.build();
     const form = document.getElementById("signForm");
     form.addEventListener("submit", handleSubmit);
-const userList = document.getElementById('users-result')
-userList.addEventListener('click',clickEsquerdo)
-userList.addEventListener('contextmenu',clickDireito)
+    const userList = document.getElementById("users-result");
+    //ADICIONADO ESCUTADOR PARA CLIQUE ESQUERDO DENTRO DA TABELA DE USUARIOS
+    userList.addEventListener("click", clickEsquerdo);
+    userList.addEventListener("contextmenu", clickDireito);
   },
 };
 
-export { controller }
+export { controller };
